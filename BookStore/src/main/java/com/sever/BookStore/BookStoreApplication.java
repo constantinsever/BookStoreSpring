997d68f39e08139ba4008f1bf774da024d3b5a82
user <user@DESKTOP-B4EM85I.mshome.net>
2020-05-18 21:57:29 +0300

    Added Book bean

--- BookStore/src/main/java/com/sever/BookStore/BookStoreApplication.java
+++ BookStore/src/main/java/com/sever/BookStore/BookStoreApplication.java
@@ -2,13 +2,39 @@ package com.sever.BookStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
-		System.out.println("start ed");
+		
+		/*
+		 * Book book1 = new Book();
+		 * 
+		 * book1.setTitle("Berurier in serai"); book1.setAuthor("San-Antonio");
+		 * 
+		 * System.out.println(book1.getAuthor() + " - " + book1.getTitle());
+		 * 
+		 * 
+		 */
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/sever/BookStore/beans.xml");
+		
+		Book book2 = (Book) context.getBean("BookBean");
+		book2.setAuthor("San Antonio");
+		
+		System.out.println(book2.getAuthor());
+		System.out.println(book2.getTitle());
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
